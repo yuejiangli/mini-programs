f2cd5789c0922c1c693d0f5b8c433d6326217712
dreamsqin <419866272@qq.com>
2024-11-06 18:53:42 +0800

    feat: request phone number and email using the obtained code

--- miniprogram/packageComponent/pages/form/button/button.js
+++ miniprogram/packageComponent/pages/form/button/button.js
@@ -11,7 +11,9 @@ const pageObject = {
     disabled: false,
     plain: false,
     loading: false,
-    canIUseGetUserProfile: false
+    canIUseGetUserProfile: false,
+    isPhoneLoading: false,
+    isEmailLoading: false
   },
 
   onShareAppMessage() {
@@ -43,12 +45,140 @@ const pageObject = {
     log(e.detail)
   },
 
+  clickGetPhoneNumber() {
+    this.setData({
+      isPhoneLoading: true
+    })
+  },
+
+  clickGetEmailAddress() {
+    this.setData({
+      isEmailLoading: true
+    })
+  },
+
   handleGetPhoneNumber(e) {
-    log('getPhoneNumber===', e.detail)
+    console.log('getPhoneNumber success===', e.detail)
+    const { code, errMsg } = e.detail
+    if (code) {
+      //发起网络请求
+      wx.request({
+        url: "https://tcmpp.woyaojianfei.club/getPhoneNumber",
+        method: "POST",
+        data: {
+          appid: "mpp0ndj6q1wqzy6d",
+          code
+        },
+        success: (res) => {
+          this.setData({
+            isPhoneLoading: false
+          })
+          console.log('getPhoneNumber request success===', res)
+          const { code = -1, data = {}, msg } = res?.data || {};
+          if (code === 200) { // 换取手机号信息成功
+            wx.showModal({
+              title: 'PhoneNumber',
+              confirmText: i18n['toast6'],
+              content: data.phone_info?.phoneNumber,
+              showCancel: false
+            })
+          } else {
+            console.log('getPhoneNumber request fail', res)
+            wx.showModal({
+              title: 'getPhoneNumber request fail',
+              confirmText: i18n['toast6'],
+              content: msg,
+              showCancel: false
+            })
+          }
+        },
+        fail: (err) => {
+          this.setData({
+            isPhoneLoading: false
+          })
+          console.log('getPhoneNumber request fail', err)
+          wx.showModal({
+            title: 'getPhoneNumber request fail',
+            confirmText: i18n['toast6'],
+            content: err.errMsg,
+            showCancel: false
+          })
+        },
+      })
+    } else {
+      this.setData({
+        isPhoneLoading: false
+      })
+      console.log('getPhoneNumber does not return code', e.detail)
+      wx.showModal({
+        title: 'getPhoneNumber fail',
+        confirmText: i18n['toast6'],
+        content: errMsg,
+        showCancel: false
+      })
+    }
   },
 
   handleGetEmailAddress(e) {
-    log('getEmailAddress===', e.detail)
+    console.log('getEmailAddress success===', e.detail)
+    const { code, errMsg } = e.detail
+    if (code) {
+      //发起网络请求
+      wx.request({
+        url: "https://tcmpp.woyaojianfei.club/getEmailAddress",
+        method: "POST",
+        data: {
+          appid: "mpp0ndj6q1wqzy6d",
+          code
+        },
+        success: (res) => {
+          this.setData({
+            isEmailLoading: false
+          })
+          console.log('getEmailAddress request success===', res)
+          const { code = -1, data = {}, msg } = res?.data || {};
+          if (code === 200) { // 换取邮箱信息成功
+            wx.showModal({
+              title: 'EmailAddress',
+              confirmText: i18n['toast6'],
+              content: data.email_info?.emailAddress,
+              showCancel: false
+            })
+          } else {
+            console.log('getEmailAddress request fail', res)
+            wx.showModal({
+              title: 'getEmailAddress request fail',
+              confirmText: i18n['toast6'],
+              content: msg,
+              showCancel: false
+            })
+          }
+        },
+        fail: (err) => {
+          this.setData({
+            isEmailLoading: false
+          })
+          console.log('getEmailAddress request fail', err)
+          wx.showModal({
+            title: 'getEmailAddress request fail',
+            confirmText: i18n['toast6'],
+            content: err.errMsg,
+            showCancel: false
+          })
+        },
+      })
+    } else {
+      this.setData({
+        isEmailLoading: false
+      })
+      console.log('getEmailAddress does not return code', e.detail)
+      wx.showModal({
+        title: 'getEmailAddress fail',
+        confirmText: i18n['toast6'],
+        content: errMsg,
+        showCancel: false
+      })
+    }
   },
 
   handleOpenSetting(e) {
